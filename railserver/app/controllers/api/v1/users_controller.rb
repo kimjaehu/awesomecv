e95afdf4d08e2be62947c833d98befa16e7f734b
weihansu <weihan.sw@gmail.com>
2019-04-06 18:57:06 +0000

    send same data structure to register and login

--- railserver/app/controllers/api/v1/users_controller.rb
+++ railserver/app/controllers/api/v1/users_controller.rb
@@ -19,7 +19,11 @@ module Api::V1
       @user = User.new(user_params)
 
       if @user.save
-        render json: @user, status: :created
+        token = JsonWebToken.encode(user_id: @user.id)
+        time = Time.now + 24.hours.to_i
+        # render json: @user, status: :created
+        render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
+                       user: @user }, status: :ok
       else
         render json: { errors: @user.errors.full_messages },
                status: :unprocessable_entity
